eb42ffeaef2b97a0983ce34c2298041e12383b54
Anita Garje <anitagarje@gmail.com>
2018-02-26 23:25:32 +0530

    added missing css

--- src/main/java/com/example/service/PassengerServiceImpl.java
+++ src/main/java/com/example/service/PassengerServiceImpl.java
@@ -147,7 +147,7 @@ public class PassengerServiceImpl implements PassengerService {
 	public String sendSMS(String PhoneNo) {
 		String OTP = "NOOTP";
 		try {
-		 /*String host = "smtp.gmail.com"; 
+		/* String host = "smtp.gmail.com"; 
 		  String user ="abusabu31@gmail.com"; 
 		  String pss = "abusabu@31"; 
 		  String to ="91"+PhoneNo+"@txtlocal.co.uk"; 
@@ -159,7 +159,7 @@ public class PassengerServiceImpl implements PassengerService {
 		  String message = "Your OTP for Mobile Verification is : " + OTP;
 		  System.out.println("message :" + message + " PhoneNo is  :" + PhoneNo);
 		
-		/* boolean sessionDebug = false; 
+		 /*boolean sessionDebug = false; 
 		  Properties pros =System.getProperties();
 		  pros.put("mail.smtp.starttls.enable", "tue");
 		  pros.put("mail.smtp.host", "host");
@@ -182,7 +182,7 @@ public class PassengerServiceImpl implements PassengerService {
 		  System.out.println("message sent successfully");
 
 		} catch (Exception ex) {
-			// JOptionPane.showMessageDialog(null, ex);
+			//JOptionPane.showMessageDialog(null, ex);
 			System.out.println("Exception is:" + ex);
 		}
 		return OTP;
